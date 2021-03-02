3a20b469597ac44c7faad2878bdc55586b3d0519
Alex Edwards <alexedwards@Alexs-MacBook-Pro.local>
2021-03-02 13:22:06 -0700

    added description drop down on card

--- front-end/src/components/Card.js
+++ front-end/src/components/Card.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+}));
 
 export default function Card(props) {
   const { img, price, name } = props;
+  const classes = useStyles();
 
   return (
     <div className="card">
@@ -9,6 +26,24 @@ export default function Card(props) {
       <h3>{name}</h3>
       <div className="cardcont">
         <h4>${price} /per day</h4>
+        <div className={classes.root}>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography className={classes.heading}>More Info</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+                eget.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        </div>
         <button>Rent Now</button>
       </div>
     </div>
