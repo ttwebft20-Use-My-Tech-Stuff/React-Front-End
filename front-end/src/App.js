eae447be9fac26a78c2c125d5a56dd987407c136
Alex Edwards <alexedwards@Alexs-MacBook-Pro.local>
2021-03-01 22:13:25 -0700

    finished card and main

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import './App.css';
-import Landing from './components/Landing';
-import Signup from './components/Signup.js'
-import Login from './components/Login';
-import { Route, Switch } from 'react-router-dom'
-
+import React from "react";
+import "./App.css";
+import Landing from "./components/Landing";
+import Signup from "./components/Signup.js";
+import Login from "./components/Login";
+import Items from "./components/Items";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-
       <Switch>
         <Route path="/signup">
           <Signup />
         </Route>
+        <Route path="/items">
+          <Items />
+        </Route>
         <Route path="/login">
           <Login />
         </Route>
-        <Route path='/'>
+        <Route path="/">
           <Landing />
         </Route>
       </Switch>
