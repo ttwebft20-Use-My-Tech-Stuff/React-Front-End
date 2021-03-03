afaffce403992f7b98340dd39c1d1246d7506723
BluKnight98 <70171431+BluKnight98@users.noreply.github.com>
2021-03-02 20:08:56 -0700

    created redux folder

---
+++ front-end/src/redux/login/loginReducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+    username: "",
+    password: "",
+    remember: false,
+}
+
+const loginReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
+
+export default loginReducer;
