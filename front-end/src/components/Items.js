17d50037921e63830d931a3168f215af5c2aed82
Alex Edwards <alexedwards@Alexs-MacBook-Pro.local>
2021-03-04 09:56:49 -0700

    fixed card row

--- front-end/src/components/Items.js
+++ front-end/src/components/Items.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { axiosWithAuth } from '../utils/axiosWithAuth';
-import ItemsList from './ItemsList';
-import ItemCard from './ItemCard';
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+import ItemsList from "./ItemsList";
+import ItemCard from "./ItemCard";
 import Navbar from "../components/Navbar";
 import placeholder from "../images/placeholder.webp";
 
@@ -10,19 +10,19 @@ export default function Items(props) {
 
   const getItemsList = () => {
     axiosWithAuth()
-      .get('/tech_items')
+      .get("/tech_items")
       .then((res) => {
         console.log(res);
         setItemsList(res.data);
       })
       .catch((err) => {
         console.log(err);
-      })
-  }
+      });
+  };
 
   useEffect(() => {
     getItemsList();
-  }, [])
+  }, []);
 
   return (
     <>
@@ -30,6 +30,7 @@ export default function Items(props) {
       <div className="itemscont">
         <div className="content-container">
           <h2>Tech Rentals. Right to your Door.</h2>
+
           <div className="content">
             <ItemsList items={itemsList} setItemsList={setItemsList} />
           </div>
