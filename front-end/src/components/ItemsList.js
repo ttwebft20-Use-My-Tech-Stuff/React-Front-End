9eb21615a2fa57190f706fbb3cac3881183a8849
BluKnight98 <70171431+BluKnight98@users.noreply.github.com>
2021-03-04 11:31:58 -0700

    redux fix

--- front-end/src/components/ItemsList.js
+++ front-end/src/components/ItemsList.js
@@ -4,20 +4,20 @@ import ItemCard from './ItemCard';
 import { fetchItems } from '../redux/itemsActions';
 
 function ItemsList({ items }) {
-  return (
-    <div className="cardrow">
-      {items.map((item) => {
-        return <ItemCard key={item.item_name} item={item} />;
-      })}
-    </div>
-  );
+    return (
+        <div className="cardrow">
+            {items.map((item) => {
+                return <ItemCard key={item.item_name} item={item} />;
+            })}
+        </div>
+    );
 }
 
-// const mapStateToProps = (state) => {
-//     return {
-//         items: state.items,
-//         isLoading: state.isLoading
-//     }
-// }
+const mapStateToProps = (state) => {
+    return {
+        items: state.items.items,
+        isLoading: state.isLoading.isLoading
+    }
+}
 
-export default connect(null, { fetchItems })(ItemsList);
+export default connect(mapStateToProps, { fetchItems })(ItemsList);
